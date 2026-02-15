6ecbef909fcf93f25ee65011b16f30c8aaa2ce72
Vivian Ta <visokoo@10-197-151-2.sds.oregonstate.edu>
2026-02-15 13:20:52 -0800

    fix(): project 3 review fixes

--- database/DML.sql
+++ database/DML.sql
@@ -8,7 +8,8 @@ GROUP 125
 /* -------------- [ Plant - SELECT, INSERT, DELETE ] -------------- */
 
 -- select all from Plant
-SELECT * FROM Plant;
+SELECT plant.plant_id AS id, plant.species, plant.plant_category AS 'plant category', plant.water_requirements AS 'water requirements', \
+       plant.sunlight, plant.season, plant.cycle, plant.edible FROM Plant;;
 
 -- insert new plant into Plant
 INSERT INTO Plant (species, plant_category, water_requirements, sunlight, season, cycle, edible)
@@ -21,31 +22,31 @@ VALUES
   :season_from_dropdownInput,
   :cycle_from_dropdownInput,
   :edible_from_radiobuttonInput
-)
+);
 
 -- delete a plant
-DELETE FROM Plant WHERE plant_id = :plant_id_selected_from_plant_page
+DELETE FROM Plant WHERE plant_id = :plant_id_selected_from_plant_page;
 
 /* -------------- [ Garden - SELECT, INSERT, DELETE ] -------------- */
 
 -- select garden_id, name, description, location, and combine the user's first/last name
 -- to show ownership of the garden
-SELECT Garden.garden_id, Garden.description, Garden.location, CONCAT(User.first_name, ' ', User.last_name) AS Owner
+SELECT Garden.garden_id AS id, Garden.description, Garden.location, CONCAT(User.first_name, ' ', User.last_name) AS owner
 FROM Garden JOIN User on Garden.user_id = User.user_id;
 
 -- select all from user but combine columns first_name and last_name as owner
-SELECT User.user_id, CONCAT(User.first_name, ' ', User.last_name) AS owner FROM USER;
+SELECT User.user_id, CONCAT(User.first_name, ' ', User.last_name) AS owner FROM User;
 
 -- delete a garden
-DELETE FROM Garden WHERE garden_id = :garden_id_selected_from_garden_page
+DELETE FROM Garden WHERE garden_id = :garden_id_selected_from_garden_page;
 
 /* -------------- [ Bed - SELECT, INSERT ] -------------- */
 
 -- get all identifying information to populate the "View All Beds" table
-SELECT * FROM Bed;
+SELECT Bed.bed_id AS id, Bed.label, Bed.length AS 'length (inches)', Bed.width AS 'width (inches)', Bed.garden_id AS garden FROM Bed;
 
 -- add a new bed
-INSERT INTO Bed (label, length, width, Garden_garden_id) 
+INSERT INTO Bed (label, length, width, Garden_garden_id);
 VALUES(:label_input, :length_input, :width_input, :garden_id_from_dropdown_input);
 
 -- get all garden_id and name to populate the Garden dropdown
@@ -54,7 +55,7 @@ SELECT garden_id, location FROM Garden ORDER BY location;
 /* -------------- [ USER - SELECT, INSERT, UPDATE, DELETE ] -------------- */
 
 -- get all identifying information to populate the "View All Users" table
-SELECT * FROM User;
+SELECT user.user_id as id, user.first_name AS 'first name', user.last_name AS 'last name' FROM User;
 
 -- add a new user
 INSERT INTO User (first_name, last_name) 
@@ -72,12 +73,12 @@ WHERE id = :user_id_selected_from_all_users_page;
 /* -------------- [ Plant_in_Bed - SELECT, INSERT, UPDATE, DELETE ] -------------- */
 
 -- get all identifying information to populate the "View All Of My Plants" table
-SELECT Plant_in_Bed.plant_id AS id, Plant.species, Plant.plant_category,
-Plant_in_Bed.date_planted, Plant_in_Bed.plant_quantity, Bed.label AS bed
-FROM Plant_in_Bed
-INNER JOIN Plant ON Plant_in_Bed.plant_id = Plant.plant_id
-INNER JOIN Bed ON Plant_in_Bed.bed_id = Bed.bed_id
-ORDER BY id
+SELECT Plant_in_Bed.plant_id AS id, Plant.species, Plant.plant_category AS 'plant category', \
+       Plant_in_Bed.date_planted AS 'date planted', Plant_in_Bed.plant_quantity AS 'plant quantity', Bed.label AS bed \
+       FROM Plant_in_Bed \
+       INNER JOIN Plant ON Plant_in_Bed.plant_id = Plant.plant_id \
+       INNER JOIN Bed ON Plant_in_Bed.bed_id = Bed.bed_id \
+       ORDER BY id;
 
 -- add a new plant in bed
 INSERT INTO Plant_in_Bed (Plant_plant_id, Bed_bed_id, date_planted, plant_quantity) 
@@ -107,7 +108,7 @@ WHERE id = :plant_in_bed_id_selected_from_all__of_my_plants_page;
 DELETE FROM Plant_in_Bed 
 WHERE id = :plant_in_bed_id_selected_from_all__of_my_plants_page;
 -- delete a plant
-DELETE FROM Plant WHERE plant_id = :plant_id_selected_from_plant_page
+DELETE FROM Plant WHERE plant_id = :plant_id_selected_from_plant_page;
 
 -- delete a garden
-DELETE FROM Garden WHERE garden_id = :garden_id_selected_from_garden_page
+DELETE FROM Garden WHERE garden_id = :garden_id_selected_from_garden_page;
